98a3124c96dfa6d01907101de96c6834a63acbb7
Strudy <proteximportsexports@gmail.com>
2017-11-19 17:44:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 GET https://api.scratch.mit.edu/proxy/featured
-Import  https://api.scratch.mit.edu/proxy/featured
+function sendAPIreq(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', 'https://api.scratch.mit.edu/proxy/featured' + projects, true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var response = xmlhttp.responseText;
+            getthumbnail(response);
+            getproxy(response);
+            getID(response)
+        }
