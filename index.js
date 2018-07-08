47c4278c1aff67c644c4bcf564c82d522e1348e8
rajesh dhiman <rajeshdhimanskt@gmail.com>
2018-07-08 21:01:11 +0530

    implements basic functinality, display html from json

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-GET https://api.scratch.mit.edu/proxy/featured
-function sendAPIreq(){
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('GET', 'https://api.scratch.mit.edu/proxy/featured' + projects, true);
-    xmlhttp.send();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            var response = xmlhttp.responseText;
-            getthumbnail(response);
-            getproxy(response);
-            getID(response)
+// GET https://api.scratch.mit.edu/proxy/featured
+(function sendAPIreq() {
+  const url = "https://api.scratch.mit.edu/proxy/featured";
+  const container = document.getElementById('result');
+  fetch(url)
+    .then(response => response.json())
+    .then(function(data) {
+        let projects = data;
+        for(var project in projects){
+            let ul = createNode('ul');
+            let title = createNode('h3');
+            title.innerHTML = project;
+            append(container, title);
+            append(container, ul);
+            projects[project].map(function(data) {
+              let li = createNode('li'),
+                  img = createNode('img'),
+                  span = createNode('span');
+              img.src = 'https:' + data.thumbnail_url;
+              span.innerHTML = `${data.title} by ${data.creator}`;
+              append(li, img);
+              append(li, span);
+              append(ul, li);
+            });
         }
-        document.getElementById("result").innerHTML = result;
+        
+    })
+})();
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
