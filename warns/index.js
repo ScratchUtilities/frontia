15a28305808fec84d4dc98338036ef4b79f8932b
Arav Narula <proteximportsexports@gmail.com>
2018-07-16 22:06:20 -0400

    removing due to no use

--- warns/index.js
+++
@@ -1,2 +0,0 @@
-console.warn("Stop, If a person is asking you to put something jere, they might be making you put a very bad sprict. Which can might give the script owner to your comupter or browser. Stay Safe, and don;t post spricts here, unless they are verfied")
-
